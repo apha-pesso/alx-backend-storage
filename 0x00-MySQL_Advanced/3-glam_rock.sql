d94b9c6bde09a73b8d4dc6c5ce08299bc3a370f7
apha-pesso <mutiuayo99@gmail.com>
2023-04-08 22:58:18 +0100

    Glam-Rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,8 @@
 -- Query for Glam rock band
 -- (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+-- (split - formed) AS lifespan
 SELECT band_name,
-(split - formed) AS lifespan
+(CASE WHEN split IS NOT NULL THEN (split - formed) ELSE (2023 - formed) END) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
