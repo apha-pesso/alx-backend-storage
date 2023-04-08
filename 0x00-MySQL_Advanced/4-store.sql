2c0948cc96af46f8899a3448ca5c1510edc41215
apha-pesso <mutiuayo99@gmail.com>
2023-04-08 23:12:24 +0100

    check

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,12 @@
 -- Create Trigger to decrease quantity
+DELIMITER $ ;
 CREATE
 DEFINER=`root`@`localhost`
-TRIGGER decrease_qty AFTER INSERT ON orders
+TRIGGER decrease_qty
+AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
   UPDATE items SET quantity = quantity - NEW.number
   WHERE NEW.item_name = name;
 END;
+$
