c11f54462fbf70d2dc85b657786c557e1af42a3e
apha-pesso <mutiuayo99@gmail.com>
2023-04-08 23:35:02 +0100

    Bonus procedure

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,16 @@
+-- Procedure for adding bonus
+-- Adds correction for a student
+DELIMITER $;
+CREATE PROCEDURE AddBonus (
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+)
+BEGIN
+    DECLARE project_id INT;
+	IF project_name NOT IN (SELECT name FROM projects) THEN
+	INSERT INTO projects (name) VALUES (project_name);
+	INSERT INTO corrections (user_id, project_id, score)
+       		VALUES (in_user_id, project_id, in_score);
+END;
+$
