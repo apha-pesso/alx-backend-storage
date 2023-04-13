c2fdf50e715321e83dbf3bf3b7a9c2a1b74014d1
apha-pesso <mutiuayo99@gmail.com>
2023-04-13 11:55:11 +0100

    Check

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,7 +6,6 @@ from pymongo import MongoClient
 if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx = client.logs.nginx
-
     log = nginx.count_documents({})
     get = nginx.count_documents({"method": "GET"})
     post = nginx.count_documents({"method": "POST"})
